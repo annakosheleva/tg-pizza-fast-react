f2cc50d44a4c26fab0a4823d166ef648a8c63ceb
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-12-02 20:28:54 -0500

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { useEffect } from "react";
 import "./App.css";
-const tg = window.Telegram.WebApp;
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
+	const { onToggleButton, tg } = useTelegram();
+
 	useEffect(() => {
 		tg.ready();
 	}, []);
 
-	return <div className="App">work</div>;
+	return (
+		<div className="App">
+			<button onClick={onToggleButton}>toggle</button>
+		</div>
+	);
 }
 
 export default App;
