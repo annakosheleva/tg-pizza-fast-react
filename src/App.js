96e94c5fac3c669eae4a8f8d0f5f7fee1c8b7f44
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-12-16 18:14:28 -0500

    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
-import ProductList from "./components/ProductList/ProductList.jsx";
-import Form from "./components/Form/Form.jsx";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
 	const { onToggleButton, tg } = useTelegram();
@@ -16,8 +16,10 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Route index element={<ProductList />} />
-			<Route path={"form"} element={<Form />} />
+			<Routes>
+				<Route index element={<ProductList />} />
+				<Route path={"form"} element={<Form />} />
+			</Routes>
 		</div>
 	);
 }
