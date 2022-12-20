bbd891b7c205b6f64885978bfe0229a5e6688bb3
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-12-19 23:38:24 -0500

    fix sendData

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -17,14 +17,14 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [city, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
